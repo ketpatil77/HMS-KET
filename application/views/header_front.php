c1acf9e8048257276d85d342d4079c30765d168a
KET ⚡ <ket.patil77@gmail.com>
2026-09-05 22:49:31 +0530

    Fix frontend CSS asset URL for Railway

--- application/views/header_front.php
+++ application/views/header_front.php
@@ -20,7 +20,7 @@ $theme = 'dark';
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@300;400;500;600;700&family=IBM+Plex+Mono:wght@400;600&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/style.css">
+    <link rel="stylesheet" href="/assets/css/style.css?v=20260905-1">
     <script>
       (function() {
         try {
